32e57f7b984b42add34d5b1803010c08eec8dbe0
dajianosh <adaji@gammastack.com>
2022-08-17 16:34:35 +0530

    Programs to be reviewed

---
+++ double_array.rb
@@ -0,0 +1,15 @@
+# doubling elemtns of array using map function
+
+arr = Array[1,2,3,4,5]
+puts "#{arr.map  {|num| num*2 } }"
+
+
+arr1 = Array[1,2,3,4,5,"Anosh"]
+double = arr1.map do |a|
+	if a.is_a?(String)
+		a= a
+	else 
+		a*2	
+	end
+end	
+puts "#{double}"
---
+++ hash_array.rb
@@ -0,0 +1,4 @@
+arr = [1,2,3,4,4,5,4,4,3,2,4,5,6,2,3,4,5,6]
+val = Hash.new(0)
+arr.each {|num| val[num]= val[num]+1}
+puts val
