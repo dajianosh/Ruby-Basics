9ed6bae42e2f8a5e7a5e0d132c2fba5a52515690
dajianosh <adaji@gammastack.com>
2022-08-18 18:48:53 +0530

    Changes made to double_array.rb

--- double_array.rb
+++ double_array.rb
@@ -1,13 +1,13 @@
 # doubling elemtns of array using map function
 arr = Array[1, 2, 3, 4, 5]
-puts "#{arr.map  {|num| num*2 } }"
+puts arr.map  {|num| num * 2 }
 # handling corner cases
-arr1 = Array[1, 2, 3, 4, 5, "Anosh"]
+arr1 = Array[1, 2, 3, 4, 5, nil]
 double = arr1.map do |a|
-  if a.is_a?(String)
-    a = a
-  else 
-    a * 2	
+  if a.is_a? Integer
+    a * 2
+  else
+    a = a 
   end
-end  
-puts "#{double}"
+end    
+puts double
