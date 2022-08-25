ac600ec4ac86065d693488d047bdc00977296e1f
dajianosh <adaji@gammastack.com>
2022-08-25 10:32:03 +0530

    Added spaces to make the code more readable

--- tax_calculations.rb
+++ tax_calculations.rb
@@ -1,4 +1,27 @@
 class Tax
+  def calculate_tax(input_details)
+    final_price = 0.0
+    total_tax = 0.0
+
+    for item in input_details
+      temp = item.split
+      quantity = Float(temp[0]) 
+      product_price = Float(temp.last)
+      product_name = temp.slice(1, temp.length - 3)
+  
+      i_tax = import_tax(product_name, product_price)
+      s_tax = sales_tax(product_name, product_price)
+  
+      total_tax += s_tax + i_tax 
+      final_price += ((product_price + s_tax + i_tax) * quantity)
+
+      puts "#{ quantity.to_i } #{ product_name.join(' ') }: #{ ((product_price + s_tax + i_tax) * quantity).round(2) }"
+    end
+
+    puts "Sales Tax: #{ (total_tax * quantity).round(2) }" 
+    puts "Total: #{ (final_price).round(2) }"
+  end   
+
   private
   def import_tax(product_name, product_price) #checking imported or not and returning import tax if applicable
     return 0.05 * product_price if product_name.include? "imported"
@@ -15,24 +38,5 @@ class Tax
     end  
     return flag ? 0.0 : (0.1 * product_price)
   end 
-  public
-  def calculate_tax(input_details)
-    final_price = 0.0
-    total_tax = 0.0
-    for item in input_details
-      temp = item.split
-      quantity = Float(temp[0]) 
-      product_price = Float(temp.last)
-      product_name = temp.slice(1, temp.length - 3)
-  
-      i_tax = import_tax(product_name, product_price)
-      s_tax = sales_tax(product_name, product_price)
-  
-      total_tax += s_tax + i_tax 
-      final_price += ((product_price + s_tax + i_tax) * quantity)
-      puts "#{ quantity.to_i } #{ product_name.join(' ') }: #{ ((product_price + s_tax + i_tax) * quantity).round(2) }"
-    end
-    puts "Sales Tax: #{ (total_tax * quantity).round(2) }" 
-    puts "Total: #{ (final_price).round(2) }"
-  end   
+
 end
